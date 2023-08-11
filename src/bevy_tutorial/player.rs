81517946897811ed74b6a6b53d24e0981bbff088
Hudi304 <hudisteanum@yahoo.com>
2023-08-11 22:51:15 +0300

    Implement basic player movement

--- src/bevy_tutorial/player.rs
+++ src/bevy_tutorial/player.rs
@@ -1,4 +1,75 @@
+use bevy::{prelude::*, window::PrimaryWindow};
 
-
-#[derive()]
+#[derive(Component)]
 pub struct Player {}
+
+pub fn spawn_player(
+    mut commands: Commands,
+    window_query: Query<&Window, With<PrimaryWindow>>,
+    asset_server: Res<AssetServer>,
+) {
+    let window = window_query.get_single().unwrap();
+
+    let transform = Transform::from_xyz(window.width() / 2.0, window.height() / 2.0, 0.0);
+
+    let texture = asset_server.load("sprites/ball.png");
+
+    commands.spawn((
+        SpriteBundle {
+            transform,
+            texture,
+            ..default()
+        },
+        Player {},
+    ));
+}
+
+pub fn spawn_camera(mut commands: Commands, window_query: Query<&Window, With<PrimaryWindow>>) {
+    let window = window_query.get_single().unwrap();
+
+    commands.spawn(Camera2dBundle {
+        transform: Transform::from_xyz(window.width() / 2.0, window.height() / 2.0, 1.0)
+            .with_scale(Vec3 {
+                x: 8.0,
+                y: 8.0,
+                z: 1.0,
+            }),
+        ..default()
+    });
+}
+
+pub const PLAYER_SPEED: f32 = 500.0;
+
+pub fn player_movement(
+    keyboard_input: Res<Input<KeyCode>>,
+    mut player_query: Query<&mut Transform, With<Player>>,
+    time: Res<Time>,
+) {
+    if let Ok(mut transform) = player_query.get_single_mut() {
+        let mut direction = Vec3::ZERO;
+
+        let left = keyboard_input.pressed(KeyCode::Left) || keyboard_input.pressed(KeyCode::A);
+        let right = keyboard_input.pressed(KeyCode::Right) || keyboard_input.pressed(KeyCode::D);
+        let up = keyboard_input.pressed(KeyCode::Up) || keyboard_input.pressed(KeyCode::W);
+        let down = keyboard_input.pressed(KeyCode::Down) || keyboard_input.pressed(KeyCode::S);
+
+        if left {
+            direction += Vec3::new(-1.0, 0.0, 0.0);
+        }
+        if right {
+            direction += Vec3::new(1.0, 0.0, 0.0);
+        }
+        if up {
+            direction += Vec3::new(0.0, 1.0, 0.0);
+        }
+        if down {
+            direction += Vec3::new(0.0, -1.0, 0.0);
+        }
+
+        if direction.length() > 0.0 {
+            direction = direction.normalize();
+        }
+
+        transform.translation += direction * PLAYER_SPEED * time.delta_seconds();
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use bevy::prelude::*;
+use bevy_tutorial::player::{player_movement, spawn_camera, spawn_player};
 use hex::{gizmos_system::gizmos_system, map::render_map};
 
+mod bevy_tutorial;
 mod common;
 mod hex;
 mod setup;
@@ -9,7 +11,7 @@ use crate::setup::setup_camera_and_walls;
 
 const BACKGROUND_COLOR: Color = Color::rgb(0.9, 0.9, 0.9);
 
-fn main() {
+fn _catan_clone() {
     App::new()
         .add_plugins(DefaultPlugins)
         .insert_resource(ClearColor(BACKGROUND_COLOR))
@@ -19,4 +21,15 @@ fn main() {
         .add_systems(Update, gizmos_system)
         .add_systems(Update, render_map)
         .run();
+}
+
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, spawn_player)
+        .add_systems(Startup, spawn_camera)
+        .add_systems(Update, player_movement)
+        .run();
+
+    return ();
 }
